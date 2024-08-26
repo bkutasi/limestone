b2bb98af6c1a4fee85a244f22d9901df75b9aa88
bkutasi <kutasi.balazs1@gmail.com>
2024-08-26 15:16:10 +0200

    main.py default settings adjusted

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bot.bot import Bot
 from bot.config import (
     BACKEND,
+    TEMPLATE,
     URI,
     BOT_USERNAME,
     DEV_ID,
@@ -16,13 +17,14 @@ if __name__ == "__main__":
     chatbot = Bot(
         token=TOKEN,
         backend=BACKEND,
+        template=TEMPLATE,
         uri=URI,
         bot_username=BOT_USERNAME,
         dev_id=DEV_ID,
         users=USERS,
         instruction_templates=instruction_templates,
-        max_new_tokens=256,
-        debug=True,
+        max_new_tokens=4096,
+        debug=False,
         database_debug=False,
         codeblock_debug=False,
         streaming=STREAMING,
